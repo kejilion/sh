5bb6097bbd8b14afe0e475bbd827a1260b422b0b
kejilion <131984541+kejilion@users.noreply.github.com>
2023-07-12 22:04:40 +0800

    备份网站目录到其他VPS

--- beifen.sh
+++ beifen.sh
@@ -5,7 +5,7 @@ cd /home/ && tar cvf web_$(date +"%Y%m%d%H%M%S").tar web
 
 # Transfer the tar archive to another VPS
 apt install -y sshpass
-cd /home/ && ls -t /home/*.tar | head -1 | xargs -I {} sshpass -p 12776 scp -P 22 {} root@142.93.15.218:/home/
+cd /home/ && ls -t /home/*.tar | head -1 | xargs -I {} sshpass -p 123456 scp -P 22 {} root@0.0.0.0:/home/
 
 # Keep only 5 tar archives and delete the rest
 cd /home/ && ls -t /home/*.tar | tail -n +4 | xargs -I {} rm {}
