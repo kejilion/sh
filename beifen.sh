7ef3598e70670ad06e605ec903e0232f3fa55aff
kejilion <131984541+kejilion@users.noreply.github.com>
2023-08-22 11:16:02 +0800

    Update beifen.sh

--- beifen.sh
+++ beifen.sh
@@ -4,7 +4,7 @@
 cd /home/ && tar czvf web_$(date +"%Y%m%d%H%M%S").tar.gz web
 
 # Transfer the tar archive to another VPS
-cd /home/ && ls -t /home/*.tar.gz | head -1 | xargs -I {} sshpass -p 123456 scp -P 22 {} root@0.0.0.0:/home/
+cd /home/ && ls -t /home/*.tar.gz | head -1 | xargs -I {} sshpass -p 123456 scp -o StrictHostKeyChecking=no -P 22 {} root@0.0.0.0:/home/
 
 # Keep only 5 tar archives and delete the rest
 cd /home/ && ls -t /home/*.tar.gz | tail -n +4 | xargs -I {} rm {}
