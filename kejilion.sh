9f3dec338c41d8161f61fc0066bbdb674c4eb6de
kejilion <131984541+kejilion@users.noreply.github.com>
2023-10-19 11:34:07 +0800

    Update kejilion.sh

--- kejilion.sh
+++ kejilion.sh
@@ -7,7 +7,7 @@ echo -e "\033[96m_  _ ____  _ _ _    _ ____ _  _ "
 echo "|_/  |___  | | |    | |  | |\ | "
 echo "| \_ |___ _| | |___ | |__| | \| "
 echo "                                "
-echo -e "\033[96m科技lion一键脚本工具 v1.8.8 （支持Ubuntu，Debian，Centos系统）\033[0m"
+echo -e "\033[96m科技lion一键脚本工具 v1.8.9 （支持Ubuntu，Debian，Centos系统）\033[0m"
 echo "------------------------"
 echo "1. 系统信息查询"
 echo "2. 系统更新"
@@ -5166,6 +5166,63 @@ case $choice in
               ;;
 
           16)
+          if dpkg -l | grep -q 'linux-xanmod'; then
+                  clear
+                  kernel_version=$(uname -r)
+                  echo "您已安装xanmod的BBRv3内核"
+                  echo "当前内核版本: $kernel_version"
+
+                  echo ""
+                  echo "内核管理"
+                  echo "------------------------"
+                  echo "1. 更新BBRv3内核              2. 卸载BBRv3内核"
+                  echo "------------------------"
+                  echo "0. 返回上一级选单"
+                  echo "------------------------"
+                  read -p "请输入你的选择: " sub_choice
+
+                  case $sub_choice in
+                      1)
+                        apt purge -y 'linux-*xanmod1*'
+                        update-grub
+
+                        apt update -y
+                        apt install -y wget gnupg
+
+                        # wget -qO - https://dl.xanmod.org/archive.key | gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg --yes
+                        wget -qO - https://raw.githubusercontent.com/kejilion/sh/main/archive.key | gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg --yes
+
+                        # 步骤3：添加存储库
+                        echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | tee /etc/apt/sources.list.d/xanmod-release.list
+
+                        # version=$(wget -q https://dl.xanmod.org/check_x86-64_psabi.sh && chmod +x check_x86-64_psabi.sh && ./check_x86-64_psabi.sh | grep -oP 'x86-64-v\K\d+|x86-64-v\d+')
+                        version=$(wget -q https://raw.githubusercontent.com/kejilion/sh/main/check_x86-64_psabi.sh && chmod +x check_x86-64_psabi.sh && ./check_x86-64_psabi.sh | grep -oP 'x86-64-v\K\d+|x86-64-v\d+')
+
+                        apt update -y
+                        apt install -y linux-xanmod-x64v$version
+
+                        echo "XanMod内核已更新。重启后生效"
+                        rm -f /etc/apt/sources.list.d/xanmod-release.list
+                        reboot
+
+                          ;;
+                      2)
+                        apt purge -y 'linux-*xanmod1*'
+                        update-grub
+                        echo "XanMod内核已卸载。重启后生效"
+                        reboot
+                          ;;
+                      0)
+                          break  # 跳出循环，退出菜单
+                          ;;
+
+                      *)
+                          break  # 跳出循环，退出菜单
+                          ;;
+                  esac
+
+        else
+
           clear
           echo "请备份数据，将为你升级Linux内核开启BBR3，仅支持Debian/Ubuntu 仅支持x86_64架构"
           echo "官网介绍: https://xanmod.org/"
@@ -5191,11 +5248,12 @@ case $choice in
               break
             fi
 
-            # 步骤1：安装系统组件
+            apt purge -y 'linux-*xanmod1*'
+            update-grub
+
             apt update -y
             apt install -y wget gnupg
 
-            # 步骤2：注册PGP密钥
             # wget -qO - https://dl.xanmod.org/archive.key | gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg --yes
             wget -qO - https://raw.githubusercontent.com/kejilion/sh/main/archive.key | gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg --yes
 
@@ -5226,6 +5284,7 @@ EOF
               echo "无效的选择，请输入 Y 或 N。"
               ;;
           esac
+        fi
               ;;
 
 
@@ -5472,6 +5531,9 @@ EOF
     echo "------------------------"
     echo "2023-10-18   v1.8.8"
     echo "系统工具中优化BBR3加速安装流程，可根据CPU型号自行安装适合的内核版本"
+    echo "------------------------"
+    echo "2023-10-19   v1.8.9"
+    echo "系统工具中BBRv3功能增加了更新内核和卸载内核功能"
     echo "------------------------"
     ;;
 
