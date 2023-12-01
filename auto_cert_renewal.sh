508a0f24a143367be1f7822afc506f5facd061da
kejilion <131984541+kejilion@users.noreply.github.com>
2023-12-01 17:36:51 +0800

    Update auto_cert_renewal.sh

--- auto_cert_renewal.sh
+++ auto_cert_renewal.sh
@@ -1,5 +1,6 @@
 # 定义证书存储目录
 certs_directory="/home/web/certs/"
+days_before_expiry=5  # 设置在证书到期前几天触发续签
 
 # 遍历所有证书文件
 for cert_file in $certs_directory*_cert.pem; do
@@ -19,10 +20,14 @@ for cert_file in $certs_directory*_cert.pem; do
     expiration_timestamp=$(date -d "${expiration_date}" +%s)
     current_timestamp=$(date +%s)
 
-    # 检查证书是否过期
-    if [ $current_timestamp -gt $expiration_timestamp ]; then
-        # 若过期，则停止 Nginx
-        echo "证书已过期，正在进行自动续签。"
+    # 计算距离过期还有几天
+    days_until_expiry=$(( ($expiration_timestamp - $current_timestamp) / 86400 ))
+
+    # 检查是否需要续签（在满足续签条件的情况下）
+    if [ $days_until_expiry -le $days_before_expiry ]; then
+        echo "证书将在${days_before_expiry}天内过期，正在进行自动续签。"
+
+        # 停止 Nginx
         docker stop nginx
 
         # 打开 iptables
@@ -43,8 +48,8 @@ for cert_file in $certs_directory*_cert.pem; do
 
         echo "证书已成功续签。"
     else
-        # 若未过期，则输出证书仍然有效
-        echo "证书仍然有效。"
+        # 若未满足续签条件，则输出证书仍然有效
+        echo "证书仍然有效，距离过期还有 ${days_until_expiry} 天。"
     fi
 
     # 输出分隔线
