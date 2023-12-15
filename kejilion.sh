ef8ec4c8840433f7c0fd0d2748ee14e2ed201e45
科技lion <131984541+kejilion@users.noreply.github.com>
2023-12-15 10:19:33 +0800

    Update kejilion.sh

--- kejilion.sh
+++ kejilion.sh
@@ -231,7 +231,7 @@ echo -e "\033[96m_  _ ____  _ _ _    _ ____ _  _ "
 echo "|_/  |___  | | |    | |  | |\ | "
 echo "| \_ |___ _| | |___ | |__| | \| "
 echo "                                "
-echo -e "\033[96m科技lion一键脚本工具 v2.0.6 （支持Ubuntu/Debian/CentOS系统）\033[0m"
+echo -e "\033[96m科技lion一键脚本工具 v2.0.7 （支持Ubuntu/Debian/CentOS系统）\033[0m"
 echo "------------------------"
 echo "1. 系统信息查询"
 echo "2. 系统更新"
@@ -4397,7 +4397,8 @@ case $choice in
       echo "16. 开启BBR3加速"
       echo "17. 防火墙高级管理器"
       echo "18. 修改主机名"
-      echo -e "19. 切换系统更新源 \033[36mBeta\033[0m"
+      echo "19. 切换系统更新源"
+      echo -e "20. 定时任务管理 \033[33mNEW\033[0m"
       echo "------------------------"
       echo "21. 留言板"
       echo "------------------------"
@@ -5639,6 +5640,57 @@ EOF
               clear
           done
 
+              ;;
+
+          20)
+          while true; do
+              clear
+              echo "定时任务列表"
+              crontab -l
+              echo ""
+              echo "操作"
+              echo "------------------------"
+              echo "1. 添加定时任务              2. 删除定时任务"
+              echo "------------------------"
+              echo "0. 返回上一级选单"
+              echo "------------------------"
+              read -p "请输入你的选择: " sub_choic
+              case $sub_choice in
+
+                  1)
+                      read -p "请输入新任务的执行命令: " newquest
+                      echo "------------------------"
+                      echo "1. 每周备份                 2. 每天备份"
+                      read -p "请输入你的选择: " dingsh
+
+                      case $dingshi in
+                          1)
+                              read -p "选择每周备份的星期几 (0-6，0代表星期日): " weekday
+                              (crontab -l ; echo "0 0 * * $weekday $newquest") | crontab - > /dev/null 2>&1
+                              ;;
+                          2)
+                              read -p "选择每天备份的时间（小时，0-23）: " hour
+                              (crontab -l ; echo "0 $hour * * * $newquest") | crontab - > /dev/null 2>&1
+                              ;;
+                          *)
+                              break  # 跳出
+                              ;;
+                      esac
+                      ;;
+
+                  2)
+                      read -p "请输入需要删除任务的关键字: " kquest
+                      crontab -l | grep -v "$kquest" | crontab -
+                      ;;
+
+                  0)
+                      break  # 跳出循环，退出菜单
+                      ;;
+                  *)
+                      break  # 跳出循环，退出菜单
+                      ;;
+              esac
+          done
               ;;
 
 
