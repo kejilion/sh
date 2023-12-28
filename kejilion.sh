4a736361f6fa9c95dc2a9efbaa949e140d2a9198
科技lion <131984541+kejilion@users.noreply.github.com>
2023-12-28 20:40:35 +0800

    Update kejilion.sh

--- kejilion.sh
+++ kejilion.sh
@@ -4096,27 +4096,9 @@ case $choice in
 
                   case $sub_choice in
                       1)
-                        apt purge -y 'linux-*xanmod1*'
-                        update-grub
-
-                        install wget gnupg
-
-                        # wget -qO - https://dl.xanmod.org/archive.key | gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg --yes
-                        wget -qO - https://raw.githubusercontent.com/kejilion/sh/main/archive.key | gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg --yes
-
-                        # 步骤3：添加存储库
-                        echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | tee /etc/apt/sources.list.d/xanmod-release.list
-
-                        # version=$(wget -q https://dl.xanmod.org/check_x86-64_psabi.sh && chmod +x check_x86-64_psabi.sh && ./check_x86-64_psabi.sh | grep -oP 'x86-64-v\K\d+|x86-64-v\d+')
-                        version=$(wget -q https://raw.githubusercontent.com/kejilion/sh/main/check_x86-64_psabi.sh && chmod +x check_x86-64_psabi.sh && ./check_x86-64_psabi.sh | grep -oP 'x86-64-v\K\d+|x86-64-v\d+')
-
                         apt update -y
-                        apt install -y linux-xanmod-x64v$version
-
+                        apt upgrade -y
                         echo "XanMod内核已更新。重启后生效"
-                        rm -f /etc/apt/sources.list.d/xanmod-release.list
-                        rm -f check_x86-64_psabi.sh*
-
                         reboot
 
                           ;;
