87e8085f8ff38c6e8efed996c6ab12c07f370299
科技lion <131984541+kejilion@users.noreply.github.com>
2024-01-12 14:30:07 +0800

    Update kejilion.sh

--- kejilion.sh
+++ kejilion.sh
@@ -2313,7 +2313,7 @@ case $choice in
       echo "21. VScode网页版                        22. UptimeKuma监控工具"
       echo "23. Memos网页备忘录                     24. pandoranext潘多拉GPT镜像站"
       echo "25. Nextcloud网盘                       26. QD-Today定时任务管理框架"
-      echo "27. Dockge容器堆栈管理面板              28. Uptime Kuma节点监控工具"
+      echo "27. Dockge容器堆栈管理面板"
       echo "------------------------"
       echo "0. 返回主菜单"
       echo "------------------------"
@@ -3401,22 +3401,6 @@ case $choice in
             docker_rum="docker run -d --name dockge --restart unless-stopped -p 5003:5001 -v /var/run/docker.sock:/var/run/docker.sock -v /home/docker/dockge/data:/app/data -v  /home/docker/dockge/stacks:/opt/stacks -e DOCKGE_STACKS_DIR=/home/docker/dockge/stacks louislam/dockge"
             docker_describe="dockge是一个可视化的docker-compose容器管理面板"
             docker_url="官网介绍: https://github.com/louislam/dockge"
-            docker_use=""
-            docker_passwd=""
-            docker_app
-              ;;
-          28)
-            docker_name="uptime-kuma"
-            docker_img="louislam/uptime-kuma:latest"
-            docker_port=3003
-            docker_rum="docker run -d \
-                              --name=uptime-kuma \
-                              -p 3003:3001 \
-                              -v /home/docker/uptime-kuma/data:/app/data \
-                              --restart=always \
-                              louislam/uptime-kuma:latest"
-            docker_describe="uptime-kuma是一款易于使用的自托管监控工具"
-            docker_url="官网介绍: https://github.com/louislam/uptime-kuma"
             docker_use=""
             docker_passwd=""
             docker_app
