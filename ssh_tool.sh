39f0226bcd06b88c80cb8f46aa3a224708c36a66
eooce <142894633+eooce@users.noreply.github.com>
2024-01-16 07:35:42 +0800

    Update ssh_tool.sh

--- ssh_tool.sh
+++ ssh_tool.sh
@@ -4804,7 +4804,7 @@ EOF
                 echo -e "${yellow}系统中未安装nodejs，正在安装nodejs...${re}"
 
                 # 根据对应系统安装最新版本的nodejs
-                curl -fsSL https://rpm.nodesource.com/setup_lts.x | sudo bash - && install nodejs
+                curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo bash - && install nodejs
                 
                 clear
                 if [ $? -eq 0 ]; then
