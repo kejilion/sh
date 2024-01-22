e83aee669968c6a984b7f7e05ce8a6b9d33b7555
eooce <142894633+eooce@users.noreply.github.com>
2024-01-22 14:55:33 +0800

    Update ssh_tool.sh

--- ssh_tool.sh
+++ ssh_tool.sh
@@ -3597,8 +3597,8 @@ case $choice in
                 while true; do
                     echo ""
                     echo -e "${purple}请保存你的密码，安装后使用该密码登录${re}"
-                    echo -e "${yellow}Linux默认用户名：${purple}root${yellow}，默认密码：${purple}LeitboGi0ro${yellow}，默认ssh端口22${re}"
-                    echo -e "${yellow}Windows默认用户名：${purple}Administrator${yellow}，默认密码：${purple}Teddysun.com${yellow}，默认远程连接端口${purple}3389${re}"
+                    echo -e "${yellow}Linux默认用户名：${purple}root${yellow} 默认密码：${purple}LeitboGi0ro${yellow} 默认ssh端口22${re}"
+                    echo -e "${yellow}Windows默认用户名：${purple}Administrator${yellow} 默认密码：${purple}Teddysun.com${yellow} 默认远程连接端口${purple}3389${re}"
                     echo -e "${yellow}詳細參數可參考Github項目地址：https://github.com/leitbogioro/Tools${re}"
                     echo ""
                     echo -e "${green}1.安装Debian-12${re}"
