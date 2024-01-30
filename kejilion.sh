5f342579e02a558c7dac7ee0fd9a74911a829b71
科技lion <131984541+kejilion@users.noreply.github.com>
2024-01-30 15:33:05 +0800

    Update kejilion.sh

--- kejilion.sh
+++ kejilion.sh
@@ -1229,7 +1229,6 @@ case $choice in
                 [Yy])
                   docker rm $(docker ps -a -q) && docker rmi $(docker images -q) && docker network prune
                   remove docker docker-ce docker-compose > /dev/null 2>&1
-                  rm -rf /var/lib/docker
                   ;;
                 [Nn])
                   ;;
