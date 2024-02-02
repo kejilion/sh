c357dd2a11270f824086a378dfaf17c7353dcbfd
科技lion <131984541+kejilion@users.noreply.github.com>
2024-02-02 12:07:07 +0800

    Update palworld.sh

--- palworld.sh
+++ palworld.sh
@@ -104,6 +104,27 @@ pal_backup() {
   curl -sS -O https://raw.githubusercontent.com/kejilion/sh/main/pal_backup.sh && chmod +x pal_backup.sh
 }
 
+
+CONTAINER_NAME="steamcmd"
+
+# 检查容器是否已安装
+if [ "$(docker ps -a -q -f name=$CONTAINER_NAME)" ]; then
+    container_status="\e[32m幻兽帕鲁已安装\e[0m"  # 绿色
+else
+    container_status="\e[90m幻兽帕鲁未安装\e[0m"  # 灰色
+fi
+
+SESSION_NAME="my1"
+
+ip_address
+# 检查 tmux 中是否存在指定的工作区
+if tmux has-session -t $SESSION_NAME 2>/dev/null; then
+    tmux_status="\e[32m已开服:\033[93m $ipv4_address:8255\e[0m"  # 绿色
+else
+    tmux_status="\e[90m未启动\e[0m"  # 灰色
+fi
+
+
 while true; do
 clear
 echo -e "\033[93m      .            .  ."
@@ -112,6 +133,7 @@ echo "[_)(_]| \/\/ (_)[  |(_]"
 echo "|                      "
 echo -e "\033[96m幻兽帕鲁私服一键脚本工具v1.0.1  by KEJILION\033[0m"
 echo -e "\033[96m-输入\033[93mp\033[96m可快速启动此脚本-\033[0m"
+echo -e "$container_status $tmux_status"
 echo "------------------------"
 echo "1. 安装幻兽帕鲁服务"
 echo "2. 启动幻兽帕鲁服务"
