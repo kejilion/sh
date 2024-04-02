6b6fbb47bac73b60924e5e9fb7649ce317465415
科技lion <131984541+kejilion@users.noreply.github.com>
2024-04-02 10:02:55 +0800

    Update kejilion.sh

--- kejilion.sh
+++ kejilion.sh
@@ -2229,6 +2229,10 @@ EOF
               echo "5. 查看SSH拦截记录                6. 查看网站拦截记录"
               echo "7. 查看防御规则列表               8. 查看日志实时监控"
               echo "------------------------"
+              echo "11. 配置拦截参数"
+              echo "------------------------"
+              echo "21. cloudflare模式"
+              echo "------------------------"
               echo "9. 卸载防御程序"
               echo "------------------------"
               echo "0. 退出"
@@ -2238,24 +2242,28 @@ EOF
                   1)
                       sed -i 's/false/true/g' /etc/fail2ban/jail.d/sshd.local
                       systemctl restart fail2ban
+                      service fail2ban restart
                       sleep 1
                       fail2ban-client status
                       ;;
                   2)
                       sed -i 's/true/false/g' /etc/fail2ban/jail.d/sshd.local
                       systemctl restart fail2ban
+                      service fail2ban restart
                       sleep 1
                       fail2ban-client status
                       ;;
                   3)
                       sed -i 's/false/true/g' /etc/fail2ban/jail.d/nginx.local
                       systemctl restart fail2ban
+                      service fail2ban restart
                       sleep 1
                       fail2ban-client status
                       ;;
                   4)
                       sed -i 's/true/false/g' /etc/fail2ban/jail.d/nginx.local
                       systemctl restart fail2ban
+                      service fail2ban restart
                       sleep 1
                       fail2ban-client status
                       ;;
@@ -2265,6 +2273,8 @@ EOF
                       echo "------------------------"
                       ;;
                   6)
+                      echo "------------------------"
+                      fail2ban-client status fail2ban-nginx-cc
                       echo "------------------------"
                       fail2ban-client status nginx-bad-request
                       echo "------------------------"
@@ -2283,12 +2293,35 @@ EOF
                       ;;
                   8)
                       tail -f /var/log/fail2ban.log
+                      break
 
                       ;;
                   9)
                       remove fail2ban
                       break
                       ;;
+
+                  11)
+                      install nano
+                      nano /etc/fail2ban/jail.d/nginx.local
+                      systemctl restart fail2ban
+                      service fail2ban restart
+                      break
+                      ;;
+                  21)
+                      echo "到cf后台右上角我的个人资料，选择左侧API令牌，获取Global API Key"
+                      echo "https://dash.cloudflare.com/login"
+                      read -p "输入CF的账号: " cfuser
+                      read -p "输入远程服务器密码: " cftoken
+
+                      cd /etc/fail2ban/action.d/
+                      curl -sS -O https://raw.githubusercontent.com/kejilion/sh/main/cloudflare.conf
+
+                      sed -i "s/kejilion@outlook.com/$cfuser/g" /etc/fail2ban/action.d/cloudflare.conf
+                      sed -i "s/APIKEY00000/$cftoken/g" /etc/fail2ban/action.d/cloudflare.conf
+                      echo "已配置cloudflare模式，可在cf后台，站点-安全性-事件中查看拦截记录"
+                      ;;
+
                   0)
                       break
                       ;;
@@ -2337,10 +2370,12 @@ EOF
           rm -rf /home/web/log/nginx/*
           docker restart nginx
 
+
           cd /etc/fail2ban/filter.d/
           curl -sS -O https://raw.githubusercontent.com/kejilion/sh/main/fail2ban-nginx-cc.conf
           cd /etc/fail2ban/jail.d/
           curl -sS -O https://raw.githubusercontent.com/kejilion/sh/main/nginx.local
+
           cd ~
           systemctl restart fail2ban
           service fail2ban restart
