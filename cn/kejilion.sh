b5a6cfc513ea23774d5fd83ac2ebad39787f389a
科技lion <131984541+kejilion@users.noreply.github.com>
2024-05-22 10:45:54 +0800

    Update kejilion.sh

--- cn/kejilion.sh
+++ cn/kejilion.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sh_v="2.5.5"
+sh_v="2.5.6"
 
 huang='\033[33m'
 bai='\033[0m'
@@ -127,8 +127,7 @@ install_add_docker() {
         rc-update add docker default
         service docker start
     else
-        # curl -fsSL https://get.docker.com | sh
-        curl -fsSL https://get.docker.com | sh -s docker --mirror Aliyun
+        curl -fsSL https://get.docker.com | sh
         systemctl start docker
         systemctl enable docker
     fi
@@ -334,7 +333,7 @@ install_ldnmp() {
 
 
 install_certbot() {
-    install certbot
+    install certbot epel-release
 
     # 切换到一个一致的目录（例如，家目录）
     cd ~ || exit
@@ -362,7 +361,9 @@ install_ssltls() {
       docker stop nginx > /dev/null 2>&1
       iptables_open
       cd ~
-      certbot certonly --standalone -d $yuming --email your@email.com --agree-tos --no-eff-email --force-renewal
+      # certbot certonly --standalone -d $yuming --email your@email.com --agree-tos --no-eff-email --force-renewal
+      certbot certonly --standalone -d $yuming --email your@email.com --agree-tos --no-eff-email --force-renewal --key-type ecdsa
+
       cp /etc/letsencrypt/live/$yuming/fullchain.pem /home/web/certs/${yuming}_cert.pem
       cp /etc/letsencrypt/live/$yuming/privkey.pem /home/web/certs/${yuming}_key.pem
       docker start nginx > /dev/null 2>&1
@@ -371,7 +372,15 @@ install_ssltls() {
 
 default_server_ssl() {
 install openssl
-openssl req -x509 -nodes -newkey rsa:2048 -keyout /home/web/certs/default_server.key -out /home/web/certs/default_server.crt -days 5475 -subj "/C=US/ST=State/L=City/O=Organization/OU=Organizational Unit/CN=Common Name"
+# openssl req -x509 -nodes -newkey rsa:2048 -keyout /home/web/certs/default_server.key -out /home/web/certs/default_server.crt -days 5475 -subj "/C=US/ST=State/L=City/O=Organization/OU=Organizational Unit/CN=Common Name"
+
+if command -v dnf &>/dev/null || command -v yum &>/dev/null; then
+    openssl req -x509 -nodes -newkey ec -pkeyopt ec_paramgen_curve:prime256v1 -keyout /home/web/certs/default_server.key -out /home/web/certs/default_server.crt -days 5475 -subj "/C=US/ST=State/L=City/O=Organization/OU=Organizational Unit/CN=Common Name"
+else
+    openssl genpkey -algorithm Ed25519 -out /home/web/certs/default_server.key
+    openssl req -x509 -key /home/web/certs/default_server.key -out /home/web/certs/default_server.crt -days 5475 -subj "/C=US/ST=State/L=City/O=Organization/OU=Organizational Unit/CN=Common Name"
+fi
+
 
 }
 
@@ -951,7 +960,8 @@ fi
 
 add_sshkey() {
 
-ssh-keygen -t rsa -b 4096 -C "xxxx@gmail.com" -f /root/.ssh/sshkey -N ""
+# ssh-keygen -t rsa -b 4096 -C "xxxx@gmail.com" -f /root/.ssh/sshkey -N ""
+ssh-keygen -t ed25519 -C "xxxx@gmail.com" -f /root/.ssh/sshkey -N ""
 
 cat ~/.ssh/sshkey.pub >> ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
@@ -1413,16 +1423,17 @@ case $choice in
       echo "------------------------"
       echo "1. 安装更新Docker环境"
       echo "------------------------"
-      echo "2. 查看Dcoker全局状态"
+      echo "2. 查看Docker全局状态"
       echo "------------------------"
-      echo "3. Dcoker容器管理 ▶"
-      echo "4. Dcoker镜像管理 ▶"
-      echo "5. Dcoker网络管理 ▶"
-      echo "6. Dcoker卷管理 ▶"
+      echo "3. Docker容器管理 ▶"
+      echo "4. Docker镜像管理 ▶"
+      echo "5. Docker网络管理 ▶"
+      echo "6. Docker卷管理 ▶"
       echo "------------------------"
       echo "7. 清理无用的docker容器和镜像网络数据卷"
+      echo "8. 更换Docker源"
       echo "------------------------"
-      echo "8. 卸载Dcoker环境"
+      echo "9. 卸载Docker环境"
       echo "------------------------"
       echo "0. 返回主菜单"
       echo "------------------------"
@@ -1436,21 +1447,21 @@ case $choice in
               ;;
           2)
               clear
-              echo "Dcoker版本"
+              echo "Docker版本"
               docker -v
               docker compose version
 
               echo ""
-              echo "Dcoker镜像列表"
+              echo "Docker镜像列表"
               docker image ls
               echo ""
-              echo "Dcoker容器列表"
+              echo "Docker容器列表"
               docker ps -a
               echo ""
-              echo "Dcoker卷列表"
+              echo "Docker卷列表"
               docker volume ls
               echo ""
-              echo "Dcoker网络列表"
+              echo "Docker网络列表"
               docker network ls
               echo ""
 
@@ -1740,6 +1751,11 @@ case $choice in
               ;;
           8)
               clear
+              bash <(curl -sSL https://linuxmirrors.cn/docker.sh)
+              ;;
+
+          9)
+              clear
               read -p "$(echo -e "${hong}确定卸载docker环境吗？(Y/N): ${bai}")" choice
               case "$choice" in
                 [Yy])
@@ -1753,9 +1769,9 @@ case $choice in
                   ;;
               esac
               ;;
+
           0)
               kejilion
-
               ;;
           *)
               echo "无效的输入!"
@@ -4448,183 +4464,176 @@ case $choice in
           }
 
 
-          root_use
-          echo "请备份数据，将为你重装系统，预计花费15分钟。"
-          echo -e "${hui}感谢MollyLau大佬和bin456789大佬的脚本支持！${bai} "
-          read -p "确定继续吗？(Y/N): " choice
+            while true; do
+              root_use
+              echo "请备份数据，将为你重装系统，预计花费15分钟。"
+              echo -e "${hui}感谢MollyLau大佬和bin456789大佬的脚本支持！${bai} "
+              echo "------------------------"
+              echo "1. Debian 12"
+              echo "2. Debian 11"
+              echo "3. Debian 10"
+              echo "4. Debian 9"
+              echo "------------------------"
+              echo "11. Ubuntu 24.04"
+              echo "12. Ubuntu 22.04"
+              echo "13. Ubuntu 20.04"
+              echo "14. Ubuntu 18.04"
+              echo "------------------------"
+              echo "21. CentOS 9"
+              echo "22. CentOS 8"
+              echo "23. CentOS 7"
+              echo "------------------------"
+              echo "31. Alpine 3.19"
+              echo "------------------------"
+              echo "41. Windows 11"
+              echo "42. Windows 10"
+              echo "43. Windows 7"
+              echo "44. Windows Server 2022"
+              echo "45. Windows Server 2019"
+              echo "46. Windows Server 2016"
+              echo "------------------------"
+              echo "0. 返回上一级选单"
+              echo "------------------------"
+              read -p "请选择要重装的系统: " sys_choice
 
-          case "$choice" in
-            [Yy])
-              while true; do
+              case "$sys_choice" in
+                1)
+                  dd_xitong_2
+                  bash InstallNET.sh -debian 12
+                  reboot
+                  exit
+                  ;;
 
-                echo "------------------------"
-                echo "1. Debian 12"
-                echo "2. Debian 11"
-                echo "3. Debian 10"
-                echo "4. Debian 9"
-                echo "------------------------"
-                echo "11. Ubuntu 24.04"
-                echo "12. Ubuntu 22.04"
-                echo "13. Ubuntu 20.04"
-                echo "14. Ubuntu 18.04"
-                echo "------------------------"
-                echo "21. CentOS 9"
-                echo "22. CentOS 8"
-                echo "23. CentOS 7"
-                echo "------------------------"
-                echo "31. Alpine 3.19"
-                echo "------------------------"
-                echo "41. Windows 11"
-                echo "42. Windows 10"
-                echo "43. Windows 7"
-                echo "44. Windows Server 2022"
-                echo "45. Windows Server 2019"
-                echo "46. Windows Server 2016"
-                echo "------------------------"
-                read -p "请选择要重装的系统: " sys_choice
+                2)
+                  dd_xitong_2
+                  bash InstallNET.sh -debian 11
+                  reboot
+                  exit
+                  ;;
 
-                case "$sys_choice" in
-                  1)
-                    dd_xitong_2
-                    bash InstallNET.sh -debian 12
-                    reboot
-                    exit
-                    ;;
+                3)
+                  dd_xitong_2
+                  bash InstallNET.sh -debian 10
+                  reboot
+                  exit
+                  ;;
+                4)
+                  dd_xitong_2
+                  bash InstallNET.sh -debian 9
+                  reboot
+                  exit
+                  ;;
 
-                  2)
-                    dd_xitong_2
-                    bash InstallNET.sh -debian 11
-                    reboot
-                    exit
-                    ;;
+                11)
+                  dd_xitong_2
+                  bash InstallNET.sh -ubuntu 24.04
+                  reboot
+                  exit
+                  ;;
+                12)
+                  dd_xitong_2
+                  bash InstallNET.sh -ubuntu 22.04
+                  reboot
+                  exit
+                  ;;
 
-                  3)
-                    dd_xitong_2
-                    bash InstallNET.sh -debian 10
-                    reboot
-                    exit
-                    ;;
-                  4)
-                    dd_xitong_2
-                    bash InstallNET.sh -debian 9
-                    reboot
-                    exit
-                    ;;
-
-                  11)
-                    dd_xitong_2
-                    bash InstallNET.sh -ubuntu 24.04
-                    reboot
-                    exit
-                    ;;
-                  12)
-                    dd_xitong_2
-                    bash InstallNET.sh -ubuntu 22.04
-                    reboot
-                    exit
-                    ;;
-
-                  13)
-                    dd_xitong_2
-                    bash InstallNET.sh -ubuntu 20.04
-                    reboot
-                    exit
-                    ;;
-                  14)
-                    dd_xitong_2
-                    bash InstallNET.sh -ubuntu 18.04
-                    reboot
-                    exit
-                    ;;
+                13)
+                  dd_xitong_2
+                  bash InstallNET.sh -ubuntu 20.04
+                  reboot
+                  exit
+                  ;;
+                14)
+                  dd_xitong_2
+                  bash InstallNET.sh -ubuntu 18.04
+                  reboot
+                  exit
+                  ;;
 
 
-                  21)
-                    dd_xitong_2
-                    bash InstallNET.sh -centos 9
-                    reboot
-                    exit
-                    ;;
+                21)
+                  dd_xitong_2
+                  bash InstallNET.sh -centos 9
+                  reboot
+                  exit
+                  ;;
 
 
-                  22)
-                    dd_xitong_2
-                    bash InstallNET.sh -centos 8
-                    reboot
-                    exit
-                    ;;
+                22)
+                  dd_xitong_2
+                  bash InstallNET.sh -centos 8
+                  reboot
+                  exit
+                  ;;
 
-                  23)
-                    dd_xitong_2
-                    bash InstallNET.sh -centos 7
-                    reboot
-                    exit
-                    ;;
+                23)
+                  dd_xitong_2
+                  bash InstallNET.sh -centos 7
+                  reboot
+                  exit
+                  ;;
 
-                  31)
-                    dd_xitong_2
-                    bash InstallNET.sh -alpine
-                    reboot
-                    exit
-                    ;;
+                31)
+                  dd_xitong_2
+                  bash InstallNET.sh -alpine
+                  reboot
+                  exit
+                  ;;
 
-                  41)
-                    dd_xitong_3
-                    bash InstallNET.sh -windows 11 -lang "cn"
-                    reboot
-                    exit
-                    ;;
+                41)
+                  dd_xitong_3
+                  bash InstallNET.sh -windows 11 -lang "cn"
+                  reboot
+                  exit
+                  ;;
 
-                  42)
-                    dd_xitong_3
-                    bash InstallNET.sh -windows 10 -lang "cn"
-                    reboot
-                    exit
-                    ;;
+                42)
+                  dd_xitong_3
+                  bash InstallNET.sh -windows 10 -lang "cn"
+                  reboot
+                  exit
+                  ;;
 
-                  43)
-                    dd_xitong_4
-                    bash reinstall.sh windows --image-name 'Windows 7 Professional' --lang zh-cn
-                    reboot
-                    exit
-                    ;;
+                43)
+                  dd_xitong_4
+                  bash reinstall.sh windows --image-name 'Windows 7 Professional' --lang zh-cn
+                  reboot
+                  exit
+                  ;;
 
-                  44)
-                    dd_xitong_4
-                    bash reinstall.sh windows --image-name 'Windows Server 2022 SERVERDATACENTER' --lang zh-cn
-                    reboot
-                    exit
-                    ;;
+                44)
+                  dd_xitong_4
+                  bash reinstall.sh windows --image-name 'Windows Server 2022 SERVERDATACENTER' --lang zh-cn
+                  reboot
+                  exit
+                  ;;
 
-                  45)
-                    dd_xitong_3
-                    bash InstallNET.sh -windows 2019 -lang "cn"
-                    reboot
-                    exit
-                    ;;
+                45)
+                  dd_xitong_3
+                  bash InstallNET.sh -windows 2019 -lang "cn"
+                  reboot
+                  exit
+                  ;;
 
-                  46)
-                    dd_xitong_3
-                    bash InstallNET.sh -windows 2016 -lang "cn"
-                    reboot
-                    exit
-                    ;;
+                46)
+                  dd_xitong_3
+                  bash InstallNET.sh -windows 2016 -lang "cn"
+                  reboot
+                  exit
+                  ;;
 
+                0)
+                  break
+                  ;;
 
-                  *)
-                    echo "无效的选择，请重新输入。"
-                    ;;
-                esac
-              done
+                *)
+                  echo "无效的选择，请重新输入。"
+                  break
+                  ;;
+              esac
+            done
+
               ;;
-            [Nn])
-              echo "已取消"
-              ;;
-            *)
-              echo "无效的选择，请输入 Y 或 N。"
-              ;;
-          esac
-              ;;
-
           9)
             root_use
 
@@ -6054,6 +6063,7 @@ EOF
             [Yy])
                 clear
                 curl -sS -O https://raw.gitmirror.com/kejilion/sh/main/kejilion.sh && chmod +x kejilion.sh
+                cp ./kejilion.sh /usr/local/bin/k > /dev/null 2>&1
                 echo -e "${lv}脚本已更新到最新版本！${huang}v$sh_v_new${bai}"
                 break_end
                 kejilion
