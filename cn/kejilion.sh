2a59dc0aa975c3f77eafd18b7f2e045a9707415e
科技lion <131984541+kejilion@users.noreply.github.com>
2024-07-09 16:56:03 +0800

    Update kejilion.sh

--- cn/kejilion.sh
+++ cn/kejilion.sh
@@ -10,8 +10,6 @@ hong='\033[31m'
 kjlan='\033[96m'
 hui='\e[37m'
 
-cp ./kejilion.sh /usr/local/bin/k > /dev/null 2>&1
-
 
 
 # 收集功能埋点信息的函数，记录当前脚本版本号，使用时间，系统版本，国家和用户使用的功能名称，绝对不涉及任何敏感信息，请放心！请相信我！
@@ -35,6 +33,57 @@ send_stats() {
          -H "Content-Type: application/json" \
          -d "{\"action\":\"$1\",\"timestamp\":\"$(date -u '+%Y-%m-%d %H:%M:%S')\",\"country\":\"$country\",\"os_info\":\"$os_info\",\"version\":\"$sh_v\"}" &>/dev/null &
 }
+
+
+
+
+
+yinsiyuanquan() {
+FILE_PATH="/usr/local/bin/k"
+VAR_NAME="ENABLE_STATS"
+
+# 从文件中读取变量值
+VAR_VALUE=$(grep "^$VAR_NAME=" "$FILE_PATH" | cut -d '=' -f2)
+
+# 去掉可能存在的引号
+VAR_VALUE=$(echo $VAR_VALUE | tr -d '"')
+
+}
+
+
+yinsiyuanquan1() {
+
+if [ "$VAR_VALUE" == "true" ]; then
+    status_message="${lv}正在采集数据${bai}"
+elif [ "$VAR_VALUE" == "false" ]; then
+    status_message="${hui}采集已关闭${bai}"
+else
+    status_message="无法确定 $VAR_NAME 的状态"
+fi
+
+}
+
+
+
+yinsiyuanquan2() {
+
+if [ "$VAR_VALUE" == "true" ]; then
+    :
+elif [ "$VAR_VALUE" == "false" ]; then
+    sed -i 's/^ENABLE_STATS=true/ENABLE_STATS=false/' /usr/local/bin/k
+    sed -i 's/^ENABLE_STATS=true/ENABLE_STATS=false/' ~/kejilion.sh
+else
+    :
+fi
+
+}
+
+
+
+yinsiyuanquan
+yinsiyuanquan2
+cp ./kejilion.sh /usr/local/bin/k > /dev/null 2>&1
+
 
 
 
@@ -1166,12 +1215,31 @@ clear
 
 dd_xitong() {
         send_stats "重装系统"
+        dd_xitong_MollyLau() {
+          country=$(curl -s ipinfo.io/country)
+          if [ "$country" = "CN" ]; then
+              wget --no-check-certificate -qO InstallNET.sh 'https://gitee.com/mb9e8j2/Tools/raw/master/Linux_reinstall/InstallNET.sh' && chmod a+x InstallNET.sh
+          else
+              wget --no-check-certificate -qO InstallNET.sh 'https://raw.gitmirror.com/leitbogioro/Tools/master/Linux_reinstall/InstallNET.sh' && chmod a+x InstallNET.sh
+          fi
+        }
+
+        dd_xitong_bin456789() {
+          country=$(curl -s ipinfo.io/country)
+          if [ "$country" = "CN" ]; then
+              curl -O https://mirror.ghproxy.com/https://raw.gitmirror.com/bin456789/reinstall/main/reinstall.sh
+          else
+              curl -O https://raw.gitmirror.com/bin456789/reinstall/main/reinstall.sh
+          fi
+        }
+
+
         dd_xitong_1() {
           echo -e "重装后初始用户名: ${huang}root${bai}  初始密码: ${huang}LeitboGi0ro${bai}  初始端口: ${huang}22${bai}"
           echo -e "按任意键继续..."
           read -n 1 -s -r -p ""
           install wget
-          wget --no-check-certificate -qO InstallNET.sh 'https://raw.gitmirror.com/leitbogioro/Tools/master/Linux_reinstall/InstallNET.sh' && chmod a+x InstallNET.sh
+          dd_xitong_MollyLau
         }
 
         dd_xitong_2() {
@@ -1179,21 +1247,21 @@ dd_xitong() {
           echo -e "按任意键继续..."
           read -n 1 -s -r -p ""
           install wget
-          wget --no-check-certificate -qO InstallNET.sh 'https://raw.gitmirror.com/leitbogioro/Tools/master/Linux_reinstall/InstallNET.sh' && chmod a+x InstallNET.sh
+          dd_xitong_MollyLau
         }
 
         dd_xitong_3() {
           echo -e "重装后初始用户名: ${huang}root${bai}  初始密码: ${huang}123@@@${bai}  初始端口: ${huang}22${bai}"
           echo -e "按任意键继续..."
           read -n 1 -s -r -p ""
-          curl -O https://raw.gitmirror.com/bin456789/reinstall/main/reinstall.sh
+          dd_xitong_bin456789
         }
 
         dd_xitong_4() {
           echo -e "重装后初始用户名: ${huang}Administrator${bai}  初始密码: ${huang}123@@@${bai}  初始端口: ${huang}3389${bai}"
           echo -e "按任意键继续..."
           read -n 1 -s -r -p ""
-          curl -O https://raw.gitmirror.com/bin456789/reinstall/main/reinstall.sh
+          dd_xitong_bin456789
         }
 
           while true; do
@@ -1201,31 +1269,22 @@ dd_xitong() {
             echo "请备份数据，将为你重装系统，预计花费15分钟。"
             echo -e "${hui}感谢MollyLau大佬和bin456789大佬的脚本支持！${bai} "
             echo "------------------------"
-            echo "1. Debian 12"
-            echo "2. Debian 11"
-            echo "3. Debian 10"
-            echo "4. Debian 9"
+            echo "1. Debian 12                  2. Debian 11"
+            echo "3. Debian 10                  4. Debian 9"
             echo "------------------------"
-            echo "11. Ubuntu 24.04"
-            echo "12. Ubuntu 22.04"
-            echo "13. Ubuntu 20.04"
-            echo "14. Ubuntu 18.04"
+            echo "11. Ubuntu 24.04              12. Ubuntu 22.04"
+            echo "13. Ubuntu 20.04              14. Ubuntu 18.04"
             echo "------------------------"
-            echo "21. CentOS 9"
-            echo "22. CentOS 8"
+            echo "21. CentOS 9                  22. CentOS 8"
             echo "23. CentOS 7"
             echo "------------------------"
-            echo "31. Alpine Linux"
-            echo "32. Rocky Linux"
-            echo "33. Alma Linux"
-            echo "34. Fedora Linux"
+            echo "31. Alpine Linux              32. Rocky Linux"
+            echo "33. Alma Linux                34. Fedora Linux"
+            echo "35. Kali Linux                36. Arch Linux"
             echo "------------------------"
-            echo "41. Windows 11"
-            echo "42. Windows 10"
-            echo "43. Windows 7"
-            echo "44. Windows Server 2022"
-            echo "45. Windows Server 2019"
-            echo "46. Windows Server 2016"
+            echo "41. Windows 11                42. Windows 10"
+            echo "43. Windows 7                 44. Windows Server 2022"
+            echo "45. Windows Server 2019       46. Windows Server 2016"
             echo "------------------------"
             echo "0. 返回上一级选单"
             echo "------------------------"
@@ -1280,20 +1339,20 @@ dd_xitong() {
                 exit
                 ;;
               21)
-                dd_xitong_1
-                bash InstallNET.sh -centos 9
+                dd_xitong_3
+                bash reinstall.sh centos 9
                 reboot
                 exit
                 ;;
               22)
-                dd_xitong_1
-                bash InstallNET.sh -centos 8
+                dd_xitong_3
+                bash reinstall.sh centos 8
                 reboot
                 exit
                 ;;
               23)
-                dd_xitong_1
-                bash InstallNET.sh -centos 7
+                dd_xitong_3
+                bash reinstall.sh centos 7
                 reboot
                 exit
                 ;;
@@ -1318,6 +1377,18 @@ dd_xitong() {
               34)
                 dd_xitong_3
                 bash reinstall.sh fedora
+                reboot
+                exit
+                ;;
+              35)
+                dd_xitong_3
+                bash reinstall.sh kali
+                reboot
+                exit
+                ;;
+              36)
+                dd_xitong_3
+                bash reinstall.sh arch
                 reboot
                 exit
                 ;;
@@ -1508,46 +1579,6 @@ EOF
 
 }
 
-
-yinsiyuanquan() {
-FILE_PATH="/usr/local/bin/k"
-VAR_NAME="ENABLE_STATS"
-
-# 从文件中读取变量值
-VAR_VALUE=$(grep "^$VAR_NAME=" "$FILE_PATH" | cut -d '=' -f2)
-
-# 去掉可能存在的引号
-VAR_VALUE=$(echo $VAR_VALUE | tr -d '"')
-
-}
-
-
-yinsiyuanquan1() {
-
-if [ "$VAR_VALUE" == "true" ]; then
-    status_message="${lv}正在采集数据${bai}"
-elif [ "$VAR_VALUE" == "false" ]; then
-    status_message="${hui}采集已关闭${bai}"
-else
-    status_message="无法确定 $VAR_NAME 的状态"
-fi
-
-}
-
-
-
-yinsiyuanquan2() {
-
-if [ "$VAR_VALUE" == "true" ]; then
-    :
-elif [ "$VAR_VALUE" == "false" ]; then
-    sed -i 's/^ENABLE_STATS=true/ENABLE_STATS=false/' /usr/local/bin/k
-    sed -i 's/^ENABLE_STATS=true/ENABLE_STATS=false/' ~/kejilion.sh
-else
-    :
-fi
-
-}
 
 
 
