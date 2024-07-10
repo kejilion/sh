12bf49b1a690a40e3033bec61dd6b96a44c163c6
科技lion <131984541+kejilion@users.noreply.github.com>
2024-07-10 20:45:37 +0800

    Update kejilion.sh

--- kejilion.sh
+++ kejilion.sh
@@ -1755,9 +1755,6 @@ case $choice in
 
     swap_info=$(free -m | awk 'NR==3{used=$3; total=$2; if (total == 0) {percentage=0} else {percentage=used*100/total}; printf "%dMB/%dMB (%d%%)", used, total, percentage}')
 
-
-    swap_info="${swap_used}MB/${swap_total}MB (${swap_percentage}%)"
-
     runtime=$(cat /proc/uptime | awk -F. '{run_days=int($1 / 86400);run_hours=int(($1 % 86400) / 3600);run_minutes=int(($1 % 3600) / 60); if (run_days > 0) printf("%d天 ", run_days); if (run_hours > 0) printf("%d时 ", run_hours); printf("%d分\n", run_minutes)}')
 
     timezone=$(current_timezone)
