c17c3e5525cc84081f2e149888b22fd4efc12eb7
科技lion <131984541+kejilion@users.noreply.github.com>
2024-07-22 15:41:06 +0800

    Update kejilion.sh

--- kejilion.sh
+++ kejilion.sh
@@ -2123,7 +2123,7 @@ clamav_scan() {
         clamav/clamav:latest \
         clamscan -r --log=/var/log/clamav/scan.log $SCAN_PARAMS
 
-    echo -e "${lv}$@ 扫描完成，病毒报告存放在/home/docker/clamav/log/scan.log ${bai}"
+    echo -e "${lv}$@ 扫描完成，病毒报告存放在${huang}/home/docker/clamav/log/scan.log ${bai}"
 
 
 }
