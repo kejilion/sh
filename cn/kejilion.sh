aef1fd08a02a5f98feb869642f667f01f3d348da
科技lion <131984541+kejilion@users.noreply.github.com>
2025-02-04 00:17:35 +0800

    Update kejilion.sh

--- cn/kejilion.sh
+++ cn/kejilion.sh
@@ -9074,6 +9074,7 @@ linux_Settings() {
 	  echo -e "${gl_kjlan}31.  ${gl_bai}切换系统语言                       ${gl_kjlan}32.  ${gl_bai}命令行美化工具 ${gl_huang}★${gl_bai}"
 	  echo -e "${gl_kjlan}33.  ${gl_bai}设置系统回收站                     ${gl_kjlan}34.  ${gl_bai}系统备份与恢复"
 	  echo -e "${gl_kjlan}35.  ${gl_bai}ssh远程连接工具                    ${gl_kjlan}36.  ${gl_bai}硬盘分区管理工具"
+	  echo -e "${gl_kjlan}37.  ${gl_bai}命令行历史记录"
 	  echo -e "${gl_kjlan}------------------------"
 	  echo -e "${gl_kjlan}41.  ${gl_bai}留言板                             ${gl_kjlan}66.  ${gl_bai}一条龙系统调优 ${gl_huang}★${gl_bai}"
 	  echo -e "${gl_kjlan}99.  ${gl_bai}重启服务器                         ${gl_kjlan}100. ${gl_bai}隐私与安全"
@@ -10037,6 +10038,10 @@ EOF
 		  36)
 			  disk_manager
 			  ;;
+		  37)
+			  send_stats "命令行历史记录"
+			  history
+			  ;;			  
 		  41)
 			clear
 			send_stats "留言板"
