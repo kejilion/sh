7617414095ad0b178882d509df22437f563d1f47
科技lion <131984541+kejilion@users.noreply.github.com>
2025-05-13 18:29:16 +0800

    Update kejilion.sh

--- kejilion.sh
+++ kejilion.sh
@@ -8340,21 +8340,20 @@ linux_panel() {
 
 			docker_rum() {
 
-
 				docker run -d \
 				  --name=qbittorrent \
 				  -e PUID=1000 \
 				  -e PGID=1000 \
 				  -e TZ=Etc/UTC \
-				  -e WEBUI_PORT=8081 \
-				  -p ${docker_port}:8081 \
-				  -p 6881:6881 \
-				  -p 6881:6881/udp \
+				  -e WEBUI_PORT=${docker_port} \
+				  -e TORRENTING_PORT=56881 \
+				  -p ${docker_port}:${docker_port} \
+				  -p 56881:56881 \
+				  -p 56881:56881/udp \
 				  -v /home/docker/qbittorrent/config:/config \
 				  -v /home/docker/qbittorrent/downloads:/downloads \
 				  --restart unless-stopped \
 				  lscr.io/linuxserver/qbittorrent:latest
-
 
 			}
 
