6936941606234b7d3440ad9f419229a4e00a8e72
科技lion <131984541+kejilion@users.noreply.github.com>
2025-12-26 17:40:51 +0800

    Simplify application listing output
    Removed app_name from output in the application listing.

--- cn/kejilion.sh
+++ cn/kejilion.sh
@@ -9333,9 +9333,8 @@ while true; do
 	  for f in "$HOME"/apps/*.conf; do
 		  [ -e "$f" ] || continue
 		  local base_name=$(basename "$f" .conf)
-		  local app_name=$(grep "app_name=" "$f" | cut -d'=' -f2 | tr -d '"' | tr -d "'")
 		  local app_text=$(grep "app_text=" "$f" | cut -d'=' -f2 | tr -d '"' | tr -d "'")
-		  echo -e "${gl_kjlan}$base_name${gl_bai} - $app_name - $app_text"
+		  echo -e "${gl_kjlan}$base_name${gl_bai} - $app_text"
 	  done
 	  echo -e "${gl_kjlan}-------------------------"
 	  echo -e "${gl_kjlan}s.   ${gl_bai}搜索三方应用                        ${gl_kjlan}d.   ${gl_bai}删除三方应用"
